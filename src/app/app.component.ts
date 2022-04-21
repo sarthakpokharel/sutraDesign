96fa3e2b8508915dfaf636345be448a4aa429f28
Sarthak Pokharel <sarthakpokharel@gmail.com>
2022-04-21 12:39:56 +0545

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'b5';
+
+  ngOnInit(): void {
+
+  //   setTimeout (() => {
+
+  //         var element = document.getElementsByClassName('nav-tabs')[0];
+
+  //   console.log(element)
+
+  //   const grid = document.createElement("i");
+  //   const crate = document.createElement("i");
+
+  //   crate.classList.add('fa fa-caret-down')
+  //   grid.classList.add('fa fa-th')
+
+  //     element.appendChild(grid);
+  //     element.appendChild(crate);
+    
+  //  }, 1000);
+  }
+
+
+  menuOpen(e:any){
+    const element: HTMLElement = e.target;
+    var ele = element.nextElementSibling
+    
+    if (ele?.classList.contains('show')){
+      ele?.classList.remove('show')
+    }
+    else{
+      ele?.classList.add('show')
+    }
+
+  }
+
+  openMenu(e:any){
+    const element: HTMLElement = e.target;
+    const ele = element.nextElementSibling?.nextElementSibling
+    const elem = element.nextElementSibling
+    // console.log(ele)
+    if (ele?.classList.contains('show') || elem?.classList.contains('show')){
+      ele?.classList.remove('show') 
+      elem?.classList.remove('show') 
+    }
+    else{
+      ele?.classList.add('show') 
+      elem?.classList.add('show')
+    }
+    
+  }
+
+  
+
 }
